5ea8eab1cc08b6560d5f6d97f037bf9d9c6fc6d0
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-03-18 10:18:05 +0530

    feat: write script to check blockNumber

---
+++ script/fetchbloch.js
@@ -0,0 +1,20 @@
+import {ethers} from "ethers"
+
+
+async function main() {
+    //frst select public node and create provider
+
+    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+    const blockNumber = await provider.getBlockNumber();
+    console.log("blockNumber : " , blockNumber.toString());   
+}
+
+//call function
+main().then((res)=>{
+    console.log("function run successfully!")
+})
+.catch((err)=>{
+    console.log("function run Error!")
+    console.log(err);
+})
+
