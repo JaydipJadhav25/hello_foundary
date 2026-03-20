71db327643ce5728499ade1d90841df666ceb56c
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-03-20 09:57:55 +0530

    feat: invariant test

---
+++ src/InvariantExample.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract InvariantExample {
+    bool public votingStatus;
+
+    function one() external{}
+    function two() external{}
+    function three() external{}
+    function four() external{}
+    function five() external{}
+    function six() external{}
+    function sev() external{
+        votingStatus=true;
+        // votingStatus=false;
+    }
+    function eig() external{}
+    function nin() external{}
+    function ten() external{}
+}
